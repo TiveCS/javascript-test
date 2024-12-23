00e0ae632e7d899edbf773279272ab8654440fcd
TiveCS <ahmadfathanhanif@gmail.com>
2024-12-23 15:29:59 +0700

    impl: add ruler and legends label

--- js/layup-drawer.js
+++ js/layup-drawer.js
@@ -55,7 +55,7 @@ function LayupDrawer() {
   /**
    * @type {Position} Initial position in pixel
    */
-  this.initialPos = { x: 40, y: 20 };
+  this.initialPos = { x: 100, y: 60 };
 }
 
 LayupDrawer.prototype = {
@@ -69,6 +69,9 @@ LayupDrawer.prototype = {
     this.ctx = canvas.getContext('2d');
     this.paralelGrainImg.src = 'images/paralel-grain-0.jpg';
     this.perpendicularGrainImg.src = 'images/perpendicular-grain-90.jpg';
+
+    this.ctx.font = '14px Arial';
+    this.ctx.strokeStyle = 'gray';
   },
 
   /**
@@ -78,8 +81,18 @@ LayupDrawer.prototype = {
    * @param {Number} length Layup length in mm
    */
   drawLayup: function (layupList, length) {
-    const grainWidth = 240;
+    const grainWidth = 200;
     const grainHeight = 150;
+
+    this.drawRuler({
+      position: { x: this.initialPos.x, y: this.initialPos.y },
+      size: { width: grainWidth + 40, height: grainHeight },
+      stepCount: 5,
+      stepGap: 20,
+      stepLength: 5,
+    });
+
+    this.drawLegends({ size: { width: grainWidth, height: grainHeight } });
 
     this.drawGrainRegion({ size: { width: grainWidth, height: grainHeight } });
 
@@ -93,7 +106,7 @@ LayupDrawer.prototype = {
         x: this.initialPos.x - 70,
         y: this.initialPos.y,
       },
-      blockSize: { width: 60, height: 30 },
+      blockSize: { width: 60, height: 25 },
     });
   },
 
@@ -230,12 +243,163 @@ LayupDrawer.prototype = {
   },
 
   /**
+   * @typedef {Object} DrawTextArgs Text drawing arguments
+   * @property {String} text Text to draw
+   * @property {Position} position Text position in pixel
+   * @property {Number} radian Text angle in radian
+   *
+   * @param {DrawTextArgs} args Drawing arguments
+   */
+  drawText: function ({ text, radian, position }) {
+    this.ctx.save();
+
+    this.ctx.translate(position.x, position.y);
+
+    this.ctx.rotate(radian);
+
+    this.ctx.textAlign = 'center';
+    this.ctx.textBaseline = 'middle';
+    this.ctx.fillText(text, 0, 0);
+
+    this.ctx.restore();
+  },
+
+  /**
+   * @typedef {Object} DrawRulerStepLineArgs Ruler step line drawing arguments
+   * @property {Number} length Step line length in pixel
+   * @property {'x' | 'y'} dir Direction of the step line
+   * @property {string?} label Step line label
+   * @property {Position} position Initial step line position in pixel
+   * @property {Number} labelOffset Label offset in pixel
+   *
+   * @param {DrawRulerStepLineArgs} args Drawing arguments
+   */
+  drawRulerStepLine: function ({
+    length,
+    dir,
+    label = null,
+    position,
+    labelOffset = 20,
+  }) {
+    this.ctx.beginPath();
+
+    this.ctx.moveTo(position.x, position.y);
+
+    this.ctx.lineTo(position.x, position.y);
+
+    if (dir === 'x') {
+      this.ctx.lineTo(position.x, position.y + length);
+    }
+
+    if (dir === 'y') {
+      this.ctx.lineTo(position.x - length, position.y);
+    }
+
+    this.ctx.stroke();
+
+    if (!label) {
+      this.ctx.closePath();
+      return;
+    }
+
+    if (dir === 'x') {
+      this.drawText({
+        text: label,
+        position: { x: position.x, y: position.y + length + labelOffset },
+        radian: -Math.PI / 4,
+      });
+    }
+
+    if (dir === 'y') {
+      this.ctx.fillText(label, position.x - length - labelOffset, position.y);
+    }
+
+    this.ctx.closePath();
+  },
+
+  /**
    * Draw layup ruler
    *
    * @typedef {Object} DrawRulerArgs Ruler drawing arguments
    * @property {Size} size Ruler size in mm
+   * @property {Position} position Ruler position in pixel
+   * @property {Number} stepGap Step gap in pixel
+   * @property {Number} stepCount Step count before the next labeled step
+   * @property {Number} stepLength Each step's length in pixel
    *
    * @param {DrawRulerArgs} args Drawing arguments
    */
-  drawRuler: function ({ size }) {},
+  drawRuler: function ({ size, position, stepCount = 5, stepLength, stepGap }) {
+    this.ctx.beginPath();
+
+    this.ctx.moveTo(position.x, position.y);
+
+    this.ctx.lineTo(position.x, position.y + this.toPixel(size.height));
+
+    this.ctx.lineTo(
+      position.x + this.toPixel(size.width),
+      position.y + this.toPixel(size.height)
+    );
+
+    this.ctx.stroke();
+
+    this.ctx.closePath();
+
+    // Draw ruler for x axis
+    for (let i = 0; i * stepGap < this.toPixel(size.width); i++) {
+      const hasLabel =
+        i % stepCount === 0 && i * stepGap < this.toPixel(size.width);
+
+      this.drawRulerStepLine({
+        label: hasLabel ? `${i * 6}` : null,
+        length: this.toPixel(stepLength),
+        dir: 'x',
+        position: {
+          x: position.x + i * stepGap,
+          y: position.y + this.toPixel(size.height),
+        },
+      });
+    }
+
+    // Draw ruler for y axis
+    for (let i = 0; i * stepGap < this.toPixel(size.height); i++) {
+      const hasLabel = i % stepCount === 0;
+
+      this.drawRulerStepLine({
+        label: hasLabel ? `${i * 12}` : null,
+        length: this.toPixel(stepLength),
+        dir: 'y',
+        position: {
+          x: position.x,
+          y: position.y + this.toPixel(size.height) - i * stepGap,
+        },
+      });
+    }
+  },
+
+  /**
+   * @typedef {Object} DrawLegendsArgs Legend drawing arguments
+   * @property {Size} size Size of layup in mm
+   *
+   * @param {DrawLegendsArgs} args
+   */
+  drawLegends: function ({ size }) {
+    this.drawText({
+      text: 'Primary Direction',
+      radian: 0,
+      position: {
+        x: this.initialPos.x + this.toPixel(size.width) / 2,
+        y: this.initialPos.y + this.toPixel(size.height) + 80,
+      },
+    });
+
+    this.drawText({
+      text: 'Slab Thickness (mm)',
+      radian: -Math.PI / 2,
+      position: {
+        x: this.initialPos.x - 60,
+        y: this.initialPos.y + this.toPixel(size.height) / 2,
+      },
+    });
+  },
 };
